7fb7c83150d053d21fcdb0da7a37ef64947d26a1
ctangam <luochenjoey@gmail.com>
2024-09-19 10:44:06 +0800

    add response

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,26 @@
 #![allow(unused_imports)]
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener};
+
+use bytes::BytesMut;
+
+struct Request {
+    length: u32,
+}
+
+struct Response {
+    header: Header,
+    body: Body,
+}
+
+struct Header {
+    length: u32,
+    correlation_id: i32,
+}
+
+struct Body {
+
+}
+
 
 fn main() {
     println!("Logs from your program will appear here!");
@@ -8,8 +29,21 @@ fn main() {
     
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
                 println!("accepted new connection");
+                let response = Response {
+                    header: Header {
+                        length: 0,
+                        correlation_id: 7,
+                    },
+                    body: Body {
+                    },
+                };
+                let mut buffer = BytesMut::new();
+                buffer.copy_from_slice(&response.header.length.to_be_bytes());
+                buffer.copy_from_slice(&response.header.correlation_id.to_be_bytes());
+        
+                stream.write(&buffer).unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
