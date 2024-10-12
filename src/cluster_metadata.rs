4376b0b7f8296b05d7cac98f6ed8c9c51c49c74e
ctangam <luochenjoey@gmail.com>
2024-10-12 11:37:59 +0800

    test

--- src/cluster_metadata.rs
+++ src/cluster_metadata.rs
@@ -129,13 +129,15 @@ enum Value {
 
 impl<T: Buf> Deserialize<T> for Value {
     fn from_bytes(buffer: &mut T) -> Self {
-        buffer.advance(1);
-        let r#type = buffer.get_u8();
-        println!("r#type: {}", r#type);
+        // buffer.advance(1);
+        // let r#type = buffer.get_u8();
+        // println!("r#type: {}", r#type);
         let mut buf = Cursor::new(buffer);
         let pos = buf.position();
+        println!("pos: {}", pos);
         buf.get_mut().advance(1);
         let r#type = buf.get_mut().get_u8();
+        println!("r#type: {}", r#type);
         buf.set_position(pos);
         
         match r#type {
